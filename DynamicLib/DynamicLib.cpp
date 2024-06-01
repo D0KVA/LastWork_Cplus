19c2e74fcade4c0785992c9482055b34d48b586f
D0KVA <isip_v.s.sokolov@mpt.ru>
2024-06-01 09:02:56 +0300

    zxc_ghoul

--- DynamicLib/DynamicLib.cpp
+++ DynamicLib/DynamicLib.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
+// Определение функции contains_all_chars
 extern "C" __declspec(dllexport) bool contains_all_chars(const char* str, const char* chars) {
+    // Создание объекта string из переданной строки str
     string s(str);
+
+    // Перебор всех символов в строке chars
     for (const char* c = chars; *c != '\0'; ++c) {
+        // Проверка, содержит ли строка s символ *c
         if (s.find(*c) == string::npos) {
+            // Если символ не найден, возвращается false
             return false;
         }
     }
+
+    // Если все символы найдены, возвращается true
     return true;
 }
--- PWC/PWC.cpp
+++ PWC/PWC.cpp
@@ -1,52 +1,67 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <string>
+#include <memory>
 
 using namespace std;
 
+// Определение типа функции, которую мы собираемся загрузить из DLL
 typedef bool (*ContainsAllCharsFunc)(const char*, const char*);
+
+// Структура для автоматического освобождения DLL
+struct DllDeleter {
+    typedef HINSTANCE pointer; // typedef для указателя на DLL
+    // Оператор(), который вызывается при удалении unique_ptr
+    void operator()(HINSTANCE hLib) const {
+        if (hLib != NULL) {
+            FreeLibrary(hLib); // Освободить DLL, если она была загружена
+        }
+    }
+};
 
 int main() {
 
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
-        string inputStr;
-        string inputChars;
+    // Устанавливаем кодировку консоли на 1251 (Windows-1251)
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-        cout << "Введите строку: ";
-        getline(cin, inputStr);
+    string inputStr; // Строка для хранения ввода пользователя
+    string inputChars; // Строка для хранения символов, которые нужно найти
 
-        cout << "Введите символы для поиска: ";
-        getline(cin, inputChars);
+    // Спрашиваем у пользователя строку и символы
+    cout << "Пожалуйста, введите строку: ";
+    getline(cin, inputStr);
 
-        cout << "Содержит ли строка: '" + inputStr + "' cимволы: '" + inputChars + "'" << endl;
+    cout << "Пожалуйста, введите символы, которые нужно найти: ";
+    getline(cin, inputChars);
 
-        HINSTANCE hLib = LoadLibrary(L"DynamicLib.dll");
-        if (hLib == NULL) {
-            cerr << "Не удалось загрузить библиотеку!" << endl;
-            return 1;
-        }
+    cout << "Проверка, содержит ли строка: '" + inputStr + "' все символы: '" + inputChars + "'" << endl;
 
-        ContainsAllCharsFunc contains_all_chars = (ContainsAllCharsFunc)GetProcAddress(hLib, "contains_all_chars");
-        if (contains_all_chars == NULL) {
-            cerr << "Не удалось получить адрес функции!" << endl;
-            FreeLibrary(hLib);
-            return 1;
-        }
+    // Загружаем DLL и оборачиваем её в умный указатель для автоматического освобождения
+    unique_ptr<HINSTANCE, DllDeleter> hLib(LoadLibrary(L"DynamicLib.dll"));
+    if (hLib == NULL) {
+        cerr << "Ошибка при загрузке библиотеки" << endl;
+        return 1; // Если DLL не загрузилась, выводим ошибку и завершаем программу
+    }
 
-        bool result = contains_all_chars(inputStr.c_str(), inputChars.c_str());
+    // Извлекаем функцию из DLL
+    ContainsAllCharsFunc contains_all_chars = (ContainsAllCharsFunc)GetProcAddress(hLib.get(), "contains_all_chars");
+    if (contains_all_chars == NULL) {
+        cerr << "Ошибка при получении адреса функции" << endl;
+        return 1; // Если функция не найдена, выводим ошибку и завершаем программу
+    }
 
-        if (result) {
-            cout << "Да!" << endl;
-        }
-        else {
-            cout << "Нет!" << endl;
-        }
+    // Вызываем функцию с введенной строкой и символами
+    bool result = contains_all_chars(inputStr.c_str(), inputChars.c_str());
 
-        FreeLibrary(hLib);
+    // Выводим результат
+    if (result) {
+        cout << "Да!" << endl;
+    }
+    else {
+        cout << "Нет!" << endl;
+    }
 
-        return 0;
+    // DLL будет автоматически освобождена при выходе из функции благодаря unique_ptr
+    return 0;
 }
-
-
-
